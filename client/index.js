d2a92f972262dcabf640bf074d56ac2c52dbeb1b
Hiroki Ito <hiroki.ito.65.38@gmail.com>
2023-05-02 00:30:15 +0900

    Add snake

--- client/index.js
+++ client/index.js
@@ -1,6 +1,40 @@
-import init, { greet } from "snake_game";
+import init, { World } from "snake_game";
 
 init().then((_) => {
-  greet("Bob");
-  console.log("OK");
+  const CELL_SIZE = 20;
+  const world = World.new();
+  const worldWidth = world.width();
+  const canvas = document.getElementById("snake-canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.height = CELL_SIZE * worldWidth;
+  canvas.width = CELL_SIZE * worldWidth;
+
+  function drawWorld() {
+    ctx.beginPath();
+
+    for (let x = 0; x < worldWidth + 1; x++) {
+      ctx.moveTo(x * CELL_SIZE, 0);
+      ctx.lineTo(x * CELL_SIZE, worldWidth * CELL_SIZE);
+    }
+
+    for (let y = 0; y < worldWidth + 1; y++) {
+      ctx.moveTo(0, y * CELL_SIZE);
+      ctx.lineTo(worldWidth * CELL_SIZE, y * CELL_SIZE);
+    }
+
+    ctx.stroke();
+  }
+
+  function drawSnake() {
+    const snakeIdx = world.snake_head_idx();
+    const col = snakeIdx % worldWidth;
+    const row = Math.floor(snakeIdx / worldWidth);
+
+    ctx.beginPath();
+    ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    ctx.stroke();
+  }
+
+  drawWorld();
+  drawSnake();
 });
--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,40 @@ use wee_alloc::WeeAlloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(name);
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+}
+
+impl Snake {
+    fn new(spawn_index: usize) -> Snake {
+        Snake {
+            body: vec![SnakeCell(spawn_index)],
+        }
+    }
 }
 
 #[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
+pub struct World {
+    width: usize,
+    snake: Snake,
+}
+
+#[wasm_bindgen]
+impl World {
+    pub fn new() -> World {
+        World {
+            width: 8,
+            snake: Snake::new(10),
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
+    }
 }
